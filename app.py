d8b3619de180e9887fa6cfb0a291c8db195dbf2c
Tan Jun Onn <98143616+hunhonn@users.noreply.github.com>
2023-12-31 01:08:10 +0800

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go 
 from streamlit_option_menu import option_menu
-import plotly.graph_objects as go
 import datetime
 import database as db
 
